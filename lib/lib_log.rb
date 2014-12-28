ab3f0d7990518b634bfa2c4ec77c32465677eaed
richbl <richbl@gmail.com>
2014-12-27 23:36:38 -0800

    changed name of lib to be more consistent

--- lib/lib_log.rb
+++ lib/lib_log.rb
@@ -5,7 +5,7 @@
 # that can be found in the LICENSE file
 #
 
-module LibLogging
+module LibLog
 
   # -----------------------------------------------------------------------------------------------
   #
